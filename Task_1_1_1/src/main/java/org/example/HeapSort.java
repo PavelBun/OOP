a4690687f95677752e61e21c982815250460a06e
PavelBun <p.bun@g.nsu.ru>
2024-09-18 17:42:48 +0700

    corrected heapsort

--- Task_1_1_1/src/main/java/org/example/HeapSort.java
+++ Task_1_1_1/src/main/java/org/example/HeapSort.java
@@ -1,41 +1,46 @@
 package org.example;
 
 public class HeapSort {
-    public void toHeap(int[] arr, int i, int len){
+    public void toHeap(int[] arr, int i, int len) {
+        //Дочерние элементы
         int left = 2 * i + 1;
         int right = 2 * i + 2;
         int larg = i;
         int buf;
-        if (left < len && arr[left] > arr[larg]){
-           larg = left;
+
+        //Находим наибольшее
+        if (left < len && arr[left] > arr[larg]) {
+            larg = left;
         }
-        if (right < len && arr[right] > arr[larg]){
-           larg = right;
+        if (right < len && arr[right] > arr[larg]) {
+            larg = right;
         }
-        if (larg != i){
+        if (larg != i) {
             buf = arr[larg];
-            arr[larg] =arr[i];
-            arr[i]= buf;
+            arr[larg] = arr[i];
+            arr[i] = buf;
             toHeap(arr, larg, len);
         }
     }
-    public int[] heapsort(int[] arr) {
-        int[] bufferArr;
-        bufferArr = new int[arr.length];
-        for (int i = 0; i < arr.length; i++){
-            bufferArr[i] = arr[i];
-        }
 
-        int len = arr.length;
-        int num_of_nodes = (len / 2) - 1;
-        for (int i = num_of_nodes; i >= 0; i--){
+    public int[] heapsort(int[] arr) {
+        int[] bufferArr = new int[arr.length];
+        System.arraycopy(arr, 0, bufferArr, 0, arr.length);
+
+        int len = bufferArr.length;
+
+        // Построение кучи
+        for (int i = (len / 2) - 1; i >= 0; i--) {
             toHeap(bufferArr, i, len);
         }
-        for(int i = len -1; i >0; i--){
+
+        // Сортировка
+        for (int i = len - 1; i > 0; i--) {
             int buf = bufferArr[i];
             bufferArr[i] = bufferArr[0];
             bufferArr[0] = buf;
-            toHeap(arr, 0, i);
+
+            toHeap(bufferArr, 0, i);
         }
         return bufferArr;
     }
