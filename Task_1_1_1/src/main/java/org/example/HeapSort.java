ec3a3cdca848da97507678ad89acd54b5f7b928f
PavelBun <p.bun@g.nsu.ru>
2024-09-18 15:08:29 +0700

    update

--- Task_1_1_1/src/main/java/org/example/HeapSort.java
+++ Task_1_1_1/src/main/java/org/example/HeapSort.java
@@ -32,19 +32,4 @@ public class HeapSort {
             toHeap(arr, 0, i);
         }
     }
-    public static void main(String[] args){
-        HeapSort Sort = new HeapSort();
-        int[] arr = {1, 3, 4 ,2, 5};
-        Sort.heapsort(arr);
-        System.out.print("[");
-        for (int i = 0; i < arr.length; i++) {
-            if (i == arr.length - 1) {
-                System.out.print(arr[i]);
-            } else {
-                System.out.print(arr[i] + ", ");
-            }
-        }
-        System.out.println("]");
-    }
-
 }
---
+++ Task_1_1_1/src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.examplec
+public class Main {
+    public static void main(String[] args){
+        HeapSort Sort = new HeapSort();
+        int[] arr = {1, 3, 4 ,2, 5};
+        Sort.heapsort(arr);
+        System.out.print("[");
+        for (int i = 0; i < arr.length; i++) {
+            if (i == arr.length - 1) {
+                System.out.print(arr[i]);
+            } else {
+                System.out.print(arr[i] + ", ");
+            }
+        }
+        System.out.println("]");
+    }
+}
