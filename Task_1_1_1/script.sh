b6c49c19fbb6b1fd162f293e1434763238b0b94e
PavelBun <p.bun@g.nsu.ru>
2024-09-24 11:07:23 +0700

    corrected script

--- Task_1_1_1/script.sh
+++ Task_1_1_1/script.sh
@@ -2,10 +2,8 @@ mkdir -p readyfiles
 #Compilation
 javac -d readyfiles src/main/java/org.example/HeapSort.java src/test/java/org.example/HeapSortTest.java
 #Generation of documentation
-javadoc src/main/java/org.example/HeapSort.java
+javadoc -d readyfiles/docs src/main/java/org/example/HeapSort.java
 #Creating jar file
-jar cfe HeapSort.jar HeapSort -C readyfiles .
+jar cfe HeapSortApp.jar org.example.HeapSort -C readyfiles .
 #start
-java -cp HeapSort.jar org.example.HeapSort
-#start tests
-java org.junit.runner.JUnitCore HeapSortTest
+java -cp HeapSortApp.jar org.example.HeapSort
