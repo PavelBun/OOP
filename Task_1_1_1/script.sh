bc6ff42df5cb94cea8001d5ac016416c464be01a
PavelBun <p.bun@g.nsu.ru>
2024-09-29 19:19:15 +0700

    changed script + main

--- Task_1_1_1/script.sh
+++ Task_1_1_1/script.sh
@@ -4,6 +4,6 @@ javadoc -d readyfiles/docs src/main/java/org/example/HeapSort.java src/main/java
 #Compilation
 javac -d readyfiles src/main/java/org/example/HeapSort.java src/main/java/org/example/Main.java
 #Creating jar file
-jar cfe HeapSortApp.jar src/main/java/org/example/Main -C readyfiles .
+jar cfe HeapSortApp.jar org.example.Main -C readyfiles .
 #start
-java  src/main/java/org/example/Main.java src/main/java/org/example/HeapSort.java
+java -jar HeapSortApp.jar
--- Task_1_1_1/src/main/java/org/example/Main.java
+++ Task_1_1_1/src/main/java/org/example/Main.java
@@ -1,15 +1,16 @@
 package org.example;
 public class Main {
-    public static void main(String[] args){
-        HeapSort Sort = new heapsortt();
-        int[] arr = {1, 3, 4 ,2, 5};
-        int[] finalarr = Sort.heapsortt(arr);
+    public static void main(String[] args) {
+        HeapSort sort = new HeapSort();
+        int[] arr = {1, 3, 4, 2, 5};
+        int[] arr1 = HeapSort.heapsort(arr);
+
         System.out.print("[");
         for (int i = 0; i < arr.length; i++) {
             if (i == arr.length - 1) {
-                System.out.print(finalarr);
+                System.out.print(arr1[i]);
             } else {
-                System.out.print(finalarr + ", ");
+                System.out.print(arr1[i] + ", ");
             }
         }
         System.out.println("]");
