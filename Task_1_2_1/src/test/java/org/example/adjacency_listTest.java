c7222b4bfb42d24c8c5fca44eb2e1dbe1cd249d8
PavelBun <144594950+PavelBun@users.noreply.github.com>
2024-11-17 19:00:58 +0700

    Update adjacency_listTest.java

--- Task_1_2_1/src/test/java/org/example/adjacency_listTest.java
+++ Task_1_2_1/src/test/java/org/example/adjacency_listTest.java
@@ -57,14 +57,66 @@ public class adjacency_listTest {
     }
 
     @Test
-    public void testTopologicalSort() {
+    public void testTopologicalSort() throws Exception {
         graph.addEdge(0, 1);
-        graph.addEdge(1, 2);
-        graph.addEdge(2, 3);
+        graph.addEdge(1, 3);
+        graph.addEdge(3, 2);
 
         List<Integer> sorted = graph.topologicalSort();
         assertEquals(4, sorted.size());
         assertEquals(0, sorted.get(0));
-        assertEquals(3, sorted.get(3));
+        assertEquals(2, sorted.get(3));
+    }
+    @Test
+    public void testCycle() throws Exception{
+        graph.addEdge(0, 1);
+        graph.addEdge(1, 2);
+        graph.addEdge(2, 3);
+        graph.addEdge(3, 0);
+        String expectedMessage = "Graph contains a cycle";
+        try {
+            List<Integer> sorted = graph.topologicalSort();
+        }
+        catch (Exception e){
+            assertEquals(expectedMessage, e.getMessage());
+        }
+
+    }
+    @Test
+    public void testReadFromFile() throws Exception {
+
+        // Читаем матрицу из файла
+        graph.readFromFile("src/test/resources/test_list.txt");
+
+        // Ожидаемая матрица
+        adjacency_list expectedMatrix = new adjacency_list(4);
+        expectedMatrix.addEdge(0, 1);
+        expectedMatrix.addEdge(1, 2);
+        expectedMatrix.addEdge(2, 3);
+        expectedMatrix.addEdge(3, 0);
+
+
+        //  равны
+        assertTrue(graph.equals(expectedMatrix));
+    }
+    @Test
+    public void testEquals_EqualMatrices() {
+
+        graph.addEdge(0, 1);
+        graph.addEdge(1, 2);
+        graph.addEdge(2, 3);
+
+        adjacency_list graph2 = new adjacency_list(4);
+        graph2.addEdge(0, 1);
+        graph2.addEdge(1, 2);
+        graph2.addEdge(2, 3);
+
+        // Проверяем, что метод equals возвращает true
+        assertTrue(graph.equals(graph2));
+
+        graph2.addEdge(0, 3);
+
+
+        assertFalse(graph.equals(graph2));
     }
 }
