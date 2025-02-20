10667177d5363d28ac5b1eb4516db920bc419f22
PavelBun <PavelBun@users.noreply.github.com>
2025-02-20 17:10:44 +0000

    Deploying to gh-pages from @ PavelBun/OOP@19ff1ade4489ec76134f0fdb69fe771853c08307 🚀

--- Task_2_1_1/type-search-index.js
+++ Task_2_1_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example","l":"ParallelPrime"},{"p":"org.example","l":"ParallelStream"},{"p":"org.example","l":"SequentialPrime"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example","l":"ParallelPrime"},{"p":"org.example","l":"ParallelStream"},{"p":"org.example","l":"PrimeChecker"},{"p":"org.example","l":"SequentialPrime"}];updateSearchResults();
