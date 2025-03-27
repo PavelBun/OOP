bfbc7ce969da8a83bc985daa4058e0dbeff5f8f7
PavelBun <PavelBun@users.noreply.github.com>
2025-03-27 20:15:20 +0000

    Deploying to gh-pages from @ PavelBun/OOP@7de6f7c88755e02abcbc521556c7775accd47bea 🚀

---
+++ Task_2_3_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example","l":"Direction"},{"p":"org.example","l":"FoodManager"},{"p":"org.example","l":"GameController"},{"p":"org.example","l":"GameLogic"},{"p":"org.example","l":"GameOverController"},{"p":"org.example","l":"MainApp"},{"p":"org.example","l":"Snake"}];updateSearchResults();
