bf0d2c284f393e60ddef7b13d2963e35b6b819c1
PavelBun <144594950+PavelBun@users.noreply.github.com>
2025-04-04 01:58:04 +0700

    Update Snake.java

--- Task_2_3_1/src/main/java/org/example/Snake.java
+++ Task_2_3_1/src/main/java/org/example/Snake.java
@@ -6,42 +6,54 @@ import javafx.geometry.Point2D;
 
 public class Snake {
     private List<Point2D> body;
-    private Direction direction;
-    private Point2D head;
+    private Direction currentDirection;
+    private Direction nextDirection; // Буфер для следующего направления
 
     public Snake(Point2D startPosition) {
         body = new ArrayList<>();
         body.add(startPosition);
-        head = startPosition;
-        direction = Direction.RIGHT;
+        currentDirection = Direction.RIGHT;
+        nextDirection = currentDirection;
     }
 
     public void setDirection(Direction newDirection) {
-        if (!direction.equals(newDirection.opposite())) {
-            direction = newDirection;
+        // Запрещаем противоположное направление
+        if (newDirection != currentDirection.opposite()) {
+            nextDirection = newDirection;
         }
     }
 
+    public void update() {
+        currentDirection = nextDirection; // Применяем направление только здесь
+        Point2D newHead = calculateNewHead();
+        body.add(0, newHead);
+        body.remove(body.size() - 1);
+    }
+
+    private Point2D calculateNewHead() {
+        Point2D head = getHead();
+        return switch (currentDirection) {
+            case UP -> new Point2D(head.getX(), head.getY() - 1);
+            case DOWN -> new Point2D(head.getX(), head.getY() + 1);
+            case LEFT -> new Point2D(head.getX() - 1, head.getY());
+            case RIGHT -> new Point2D(head.getX() + 1, head.getY());
+        };
+    }
+
+    // Геттеры
     public Direction getDirection() {
-        return direction;
+        return currentDirection;
     }
 
     public Point2D getHead() {
-        return head;
+        return body.get(0);
     }
 
     public List<Point2D> getBody() {
         return body;
     }
 
-    public void move(Point2D newHead) {
-        body.add(0, newHead);
-        head = newHead;
-        body.remove(body.size() - 1);
-    }
-
     public void grow(Point2D newHead) {
         body.add(0, newHead);
-        head = newHead;
     }
 }
