0b325ad88796e1c4d46fcaa9b9202627a742cf3c
PavelBun <144594950+PavelBun@users.noreply.github.com>
2025-04-24 19:23:58 +0700

    Update build.gradle.kts

--- Task_2_4_1/build.gradle.kts
+++ Task_2_4_1/build.gradle.kts
@@ -14,11 +14,28 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-scripting-jsr223:1.9.22")
     implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.9.1")
     implementation("com.github.spotbugs:spotbugs:4.8.2")
-    implementation("org.junit.platform:junit-platform-console:1.10.1")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.1") // Добавлено
 }
 
 application {
     mainClass.set("MainKt")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport) // Добавлено
+}
+
+jacoco {
+    toolVersion = "0.8.11"
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(true)
+        xml.outputLocation.set(layout.buildDirectory.file("reports/jacoco/test/jacocoTestReport.xml")) // Добавлено
+        html.required.set(true)
+    }
 }
 
 kotlin {
@@ -27,14 +44,4 @@ kotlin {
 
 tasks.withType<JavaCompile> {
     targetCompatibility = "21"
-}
-jacoco {
-    toolVersion = "0.8.11" 
-}
-tasks.jacocoTestReport {
-    reports {
-        xml.required.set(true)
-        html.required.set(true)
-        csv.required.set(false)
-    }
 }
